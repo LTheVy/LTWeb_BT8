39753b1a5902be8c2123df0dc854f0c73c33a28b
Lục Thế Vỹ <23110368@student.hcmute.edu.vn>
2025-09-29 20:38:53 +0700

    Push lại

---
+++ src/main/java/vn/iostar/service/impl/FileUploadServiceImpl.java
@@ -0,0 +1,35 @@
+package vn.iostar.service.impl;
+
+import vn.iostar.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileUploadServiceImpl implements FileUploadService {
+
+    @Value("${upload.path}")
+    private String uploadDir;
+
+    @Override
+    public String storeFile(MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            throw new IOException("File is empty");
+        }
+
+        String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        Path path = Paths.get(uploadDir, filename);
+
+        Files.createDirectories(path.getParent());
+
+        file.transferTo(path.toFile());
+
+        return filename;
+    }
+}
